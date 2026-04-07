7dc8cc45a3b6b71802148ee834ffcde6b12ee8e9
Lucas Alexandre <lucasparaoif@gmail.com>
2026-04-06 23:27:59 -0300

    feat: add StackArray

---
+++ stack/stack_array/StackArray.java
@@ -0,0 +1,52 @@
+package stack.stack_array;
+
+import utils.CustomException;
+
+public class StackArray<T> {
+
+  private final int capacity;
+  private final Object[] elements;
+  private int top;
+
+  public StackArray(int capacity) {
+    this.capacity = capacity;
+    top = -1;
+    elements = new Object[capacity];
+  }
+  
+  public boolean isFull() {
+    return top == capacity - 1;
+  }  
+
+  public boolean isEmpty() {
+    return top == -1;
+  }
+
+  public int size() {
+    return top + 1;
+  }
+
+  public void push(T element) throws CustomException {
+    if (isFull()) throw new CustomException("ERROR: Stack is full!");
+
+    elements[++top] = element;
+  }
+
+  @SuppressWarnings("unchecked")
+  public T pop() throws CustomException {
+    if (isEmpty()) throw new CustomException("ERROR: Stack is empty!");
+
+    T element = (T) elements[top];
+    elements[top--] = null;
+
+    return element;
+  }
+  
+  @SuppressWarnings("unchecked")
+  public T peek() throws CustomException {
+    if (isEmpty()) throw new CustomException("ERROR: Stack is empty!");
+
+    return (T) elements[top];
+  }
+
+}
